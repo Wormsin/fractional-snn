aed12d5fd5c7d35cc2286a42fb49b54175baba92
wormsin <bobdomeme@gmail.com>
2024-02-28 13:24:54 +0300

    fix

--- process_plot.py
+++ process_plot.py
@@ -5,6 +5,7 @@ import csv
 from scipy.special import gamma 
 import numfracpy as nfr
 from scipy.optimize import curve_fit
+from scipy.integrate import quad
 
 mpl.rcParams['pdf.fonttype'] = 42
 mpl.rcParams['ps.fonttype'] = 42
@@ -19,52 +20,60 @@ def extract_data(file_name):
     return X, Y
 
 def plot_data(ax, x, y, log):
-    p = ax.plot(x, y, marker ='o', markersize = 8, linewidth = 0)
+    p = ax.plot(x, y, marker ='o',markerfacecolor='white', markersize = 8, linewidth = 0)
     if log:
-        ax.yscale('log')
-        ax.xscale('log')
+        ax.set_yscale('log')
+        ax.set_xscale('log')
     return p
 
 def get_model_plot(ax, x, y, log):
-    p = ax.plot(x, y, linewidth = 3, zorder = 3)
+    p = ax.plot(x, y, linewidth = 3)
     if log:
-        ax.yscale('log')
-        ax.xscale('log')
+        ax.set_yscale('log')
+        ax.set_xscale('log')
     return p
 
-def mittag_leffler(T, alpha, tau_m):
-    MLF = []
+def mittag_leffler(T, A, alpha):
+    mlf_arr = []
     for t in T:
-        mlf = 0.1*nfr.Mittag_Leffler_one(-((t)**(alpha))/tau_m, alpha)
-        MLF.append(mlf)
-    return np.array(MLF)
-
-def plot_model(ax, x, y, p0):
-    params, covariance = curve_fit(mittag_leffler, x, y, p0)
-    fit = mittag_leffler(x, *params)
-    print(*params, covariance)
-    pm = get_model_plot(ax, x, fit, False)
-    return pm, *params
-
-file_name = "data_tio2_n.csv"
-xn, yn,  = extract_data(file_name)
-file_name = "data_tio2.csv"
-x, y,  = extract_data(file_name)
-
-fig = plt.figure(figsize=(10, 6))
-ax = fig.add_axes([0.1,0.1,0.5,0.8])
-ax.set_xlabel('t, mcs', fontweight = 'bold', size = 10)
-ax.set_ylabel('I(V)', fontweight = 'bold', size = 10)
+        mlf = nfr.Mittag_Leffler_one(-(t/T[0])**(alpha), alpha)*A/(nfr.Mittag_Leffler_one(-(1)**(alpha), alpha))
+        #mlf = A*nfr.Mittag_Leffler_one(-((t-T[0])/1e-6)**(alpha), alpha)/nfr.Mittag_Leffler_one(-((T[1]-T[0])/1e-6)**(alpha), alpha)
+        mlf_arr.append(mlf)
+    return np.array(mlf_arr)
 
 
-pm, alfa, _  = plot_model(ax,x[x>x[np.argmax(y)]], y[x>x[np.argmax(y)]], [0.8, 2])
-pmn, alfan, _ = plot_model(ax, xn[xn>0], yn[xn>0], [0.5, 2])
+def plot_model(ax, x, y, A, p0):
+    #params, covariance = curve_fit(lambda x, alpha: mittag_leffler(x, A, alpha), x, y, p0)
+    params = p0[0]
+    fit = mittag_leffler(x, A, params)
+    print(params)
+    pm = get_model_plot(ax, x, fit, True)
+    return pm, params
 
-#fit = mittag_leffler(x[x>x[np.argmax(y)]], 0.079, 0.005)
-#pm = get_model_plot(ax, x[x>x[np.argmax(y)]], fit, False)
+def creat_fig(data, names, fit):
+    fig = plt.figure(figsize=(10, 6))
+    ax = fig.add_axes([0.1,0.1,0.5,0.8])
+    ax.set_xlabel('t, mcs', fontweight = 'bold', size = 10)
+    ax.set_ylabel('Normalized TRMC singnal', fontweight = 'bold', size = 10)
+    p_arr = []
+    alfa_arr = []
+    pm_arr = []
+    for file_name in data:
+        x, y,  = extract_data(file_name)
+        p = plot_data(ax, x, y, True)
+        p_arr.append(p[0])
+        if fit:
+            pm, alfa = plot_model(ax,x[x>x[np.argmax(y)]], y[x>x[np.argmax(y)]], np.max(y), [0.35])
+            #pm, alfa = plot_model(ax,x[x>9e-8], y[x>9e-8], np.max(y[x>9e-8]), [0.35])
+            pm_arr.append(pm[0])
+            alfa_arr.append(f'mlf, alpha = {np.round(alfa, 2)}')
+    p_arr = p_arr+pm_arr      
+    names = names + alfa_arr
+    ax.legend(handles = p_arr, labels = names)
+    ax.grid(True,which='major',axis='both',alpha=0.3)
+    plt.show()
 
-pn = plot_data(ax, xn, yn, False)
-p = plot_data(ax, x, y, False)
-ax.legend(handles = [pn[0], p[0], pmn[0], pm[0]], labels = ['N-TiO2', 'TiO2', f'alpha = {np.round(alfan, 2)}', f'alpha = {np.round(alfa, 3)}'])
-ax.grid(True,which='major',axis='both',alpha=0.3)
-plt.show()
+
+file_names = ["data_bare.csv"]
+labels = ["bare film 20nm"]
+creat_fig(file_names, labels, True)
