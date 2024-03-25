a87eda976907a42121cc49b25c2b696cb98786f4
wormsin <bobdomeme@gmail.com>
2024-03-25 18:42:19 +0300

    fix

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ import utils
 import proc
 import model as m
 import numpy as np
-import os
-from shutil import copyfile
 import matplotlib.pyplot as plt
-from scipy.special import gamma 
-
-
 
 
 pars = {'V_th': -50.0, 'V_reset': -70.0, 'g_L': 25.0, 'C_m': 500,  'V_init': -70.0, 'E_L': -70.0, 'tref': 5.0, 'dt': 0.1,
@@ -29,11 +24,11 @@ stdp_rate = pars['stdp_rate']
 
 start_V = E_L
 time_step = 5 # ms
-num_inputs = 1
-nu = np.array([0.2])
+nu = np.array([0.4, 0.6])
 alfa = 0.4
-N = Lt//(4*time_step)
-input = np.array([1])
+N = Lt*dt//(4*time_step)
+input = np.array([1, 1])
+num_inputs = len(input)
 neuron = m.Fractional_LIF(V_th, V_reset, E_L, Iinj, g_L, C_m, dt, alfa, tref, stdp_rate, tm, tp, Am, Ap)
 proc = proc.Process(V_th, E_L, dt, Lt, range_t, N, nu, time_step, Iinj, alfa, num_inputs, input, neuron)
 
@@ -42,7 +37,7 @@ proc.instant_view()
 #proc.make_new_data(5)
 #proc.add_data(5, 4)
 
-#proc.plot_file("plot/data_4.csv", False)
+#proc.plot_file("04_06/data_4.csv", False)
 #proc.plot_folder("output_data", False)
 #proc.plot_files('output_data')
 
