3966a0eed2a811a1ba6bcc81aa2ed170ecd19a84
wormsin <bobdomeme@gmail.com>
2024-04-03 09:29:38 +0300

    update main file

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import utils
-import proc
-import model as m
 import numpy as np
 import matplotlib.pyplot as plt
 
+from model import neuron, layer, fc_model, train, eval
+from data_proc import encoding
 
 pars = {'V_th': -50.0, 'V_reset': -70.0, 'g_L': 25.0, 'C_m': 500,  'V_init': -70.0, 'E_L': -70.0, 'tref': 5.0, 'dt': 0.1,
         'tm':5, 'tp':3, 'Ap':0.6, 'Am':-0.3, 'range_t': np.arange(0, 1000, 0.1), 'Iinj': 3, 'stdp_rate': 0.0625}
@@ -23,29 +22,14 @@ stdp_rate = pars['stdp_rate']
 
 start_V = E_L
 time_step = 0.1 # ms
-nu = np.array([0.2, 0.6])
 alfa = 0.5
 N = range_t[-1]//(time_step)
-input = np.array([1, 1])
-num_inputs = len(input)
-neuron = m.Fractional_LIF(V_th, V_reset, E_L, Iinj, g_L, C_m, dt, alfa, tref, stdp_rate, tm, tp, Am, Ap)
-proc = proc.Process(V_th, E_L, dt, Lt, range_t, N, nu, time_step, Iinj, alfa, num_inputs, input, neuron)
 
-#proc.instant_view()
-#proc.make_new_data(10, True)
-#proc.add_data(20, 10, True)
 
-#proc.plot_file("output_data/data_0.csv", False)
-proc.plot_folder("output_data", False)
-#proc.plot_files('output_data')
+neuron_flif = neuron.Fractional_LIF(V_th, V_reset, start_V, Iinj, g_L, C_m, dt, alfa, tref)
+layer1 = layer.Layer(9, 3, start_V, neuron_flif)
+layer2 = layer.Layer(3, 2, start_V, neuron_flif)
+snn = fc_model.FC([layer1, layer2], Lt)
 
-#utils.plot_voltage_memory_trace(["vmt/nu09/05","vmt/nu09/09"])
-#utils.plot_weights(dir="output_data", L = 10, nu = nu, alfa=alfa)
-'''
-ax = utils.a4_plot()
-ax = utils.ISI_plot(ax, ["output_data"], 10, 3000, 0)
-#ax = utils.ISI_plot(ax, "08_02_1", 4, 1500, 0.5)
-#utils.ISI_plot(ax, "plot", 8, 1500, 1)
-plt.legend()
-plt.show()
-'''
+
+#train.train(model=snn, epochs=50, data=data, N_spk=N, dt=dt, t_step=time_step)
